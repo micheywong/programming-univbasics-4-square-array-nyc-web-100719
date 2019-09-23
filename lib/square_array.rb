ece03777c86644579df0b2c6e2783ceba619da50
Michelle Wong <michellekwong2@gmail.com>
2019-09-23 16:29:12 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,7 @@ def square_array(array)
   counter = 0
 
  while counter < array.length do
-* 		    puts array[counter]
-* 		    counter += 1
-* 		  end
-* 		end
+  puts array[counter]
+  counter += 1
+ end
 end
